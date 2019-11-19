887f13c53ef58e9415c57d7230fbdb982631c7e0
Nate-Hermes <54755084+Nate-Hermes@users.noreply.github.com>
2019-11-18 23:43:07 -0600

    Loaded in all tile assets to the game and finished setting up the board size.

--- JavaScript/game.js
+++ JavaScript/game.js
@@ -1,11 +1,20 @@
 var game;
+//gameOption holds all the specific game data variables
+var gameOptions = {
+	tileSize: 200,
+	tileSpacing: 20,
+	boardSize: {
+		rows: 4,
+		cols: 4,
+	}
+}
 
 // Create the window for the game to run in.
 window.onload = function() {
 	var gameConfig = {
-		width:480,
-		height:640,
-		backgroubdColor: 0xff0000,
+		width: gameOptions.boardSize.cols * (gameOptions.tileSize + gameOptions. tileSpacing) + gameOptions.tileSpacing,
+		height: gameOptions.boardSize.rows * (gameOptions.tileSize + gameOptions. tileSpacing) + gameOptions.tileSpacing,
+		backgroundColor: 0xecf0f1,
 		scene:[bootGame, playGame]
 		
 	}
@@ -36,6 +45,15 @@ class bootGame extends Phaser.Scene {
 	constructor() {
 		super("BootGame");
 	}
+	
+	preload() {
+		this.load.image("emptytile", "assets/sprites/emptytile.png");
+		this.load.spritesheet("tiles", "assets/sprites/tiles.png", {
+			frameWidth: gameOptions.tileSize,
+			frameHeight: gameOptions.tileSize,
+		});
+	}
+	
 	create() {
 		console.log("game is booting....");
 		this.scene.start("PlayGame");
@@ -47,6 +65,48 @@ class playGame extends Phaser.Scene {
 		super("PlayGame");
 	}
 	create() {
-		console.log("This is my awesome game!");
+		this.boardArray = [];
+		
+		
+	for(var i =0; i<gameOptions.boardSize.rows; i++){
+		
+		this.boardArray[i] = [];
+		
+		for(var j = 0; j<gameOptions.boardSize.cols; j++){
+		var tilePosition = this.getTilePosition(i, j);
+		this.add.image(tilePosition.x, tilePosition.y, "emptytile");
+			
+		var tile = this.add.sprite(tilePosition.x, tilePosition.y, "tiles", 0);
+			tile.visible = false;
+			
+			this.boardArray[i][j] = {
+				tileValue: 0,
+				tileSprite: tile,
+			}
+		}
 	}
 }
+
+	getTilePosition(row, col) {
+		var posX = gameOptions.tileSpacing * (col + 1) + gameOptions.tileSize * (col + 0.5);
+		var posY = gameOptions.tileSpacing * (row + 1) + gameOptions.tileSize * (row + 0.5);
+		return new Phaser.Geom.Point(posX, posY);
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
